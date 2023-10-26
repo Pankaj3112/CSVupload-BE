3dd7bfdb02d240c8a871b08f9c14e9fbeaf53250
Pankaj <pankajbeniwal3112@gmail.com>
2023-10-26 13:40:49 +0530

    add cors config

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
-const db = require('./config/mongoose');
-const upload = require('express-fileupload');
-const path = require('path');
+const db = require("./config/mongoose");
+const upload = require("express-fileupload");
+const path = require("path");
 
 //parser for form data
-app.use(express.urlencoded(
-    {
-        extended: true
-    }
-));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 //using express-fileupload
 app.use(upload());
 
+// Allow all origins
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 //routes
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 //fire up the server
-app.listen(port, function(err){
-    if(err){
-        console.log(`Error in running the server: ${err}`);
-    }
-    console.log(`****Server fired up on port: ${port}****`);
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+  console.log(`****Server fired up on port: ${port}****`);
 });
